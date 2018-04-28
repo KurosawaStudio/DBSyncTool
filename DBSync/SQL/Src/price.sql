d45538cd90d4ca16e9440ea9b7cfd90f107cdc1f
Yuki Takeya <yuki@gakkougurashi.net>
2018-04-28 17:59:06 +0900

    Fix SQL

--- DBSync/SQL/Src/price.sql
+++ DBSync/SQL/Src/price.sql
@@ -1,3 +1,2 @@
-﻿select SPBM as drugCode,PRICE04 as price FROM jc_jgtx where spbm in 
-(select spbm FROM jc_spxx WHERE (SDYW=0 or SDYW is null) and 
-(ABCFL Like 'KS/%' or ABCFL like 'ZH/%' or ABCFL like 'BQ/YSB%' or dl=02)) --and spbm='00120510'
+﻿SELECT jc_spxx.spbm as drugCode,pzpj as price
+FROM jc_spfl,jc_spxx,jc_pck WHERE fl=flbm and jc_spxx.spbm=jc_pck.spbm and (LDW='FZG' OR LDW='JMGS')
