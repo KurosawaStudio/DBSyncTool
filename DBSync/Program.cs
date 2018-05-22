8c8b4b9dd873ecc33f17fe82fc8d7c4776c1b455
Yuki Takeya <yuki@gakkougurashi.net>
2018-05-22 19:40:06 +0800

    Downgrade to 4.0

--- DBSync/Program.cs
+++ DBSync/Program.cs
@@ -5,7 +5,7 @@ using System.Drawing;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Security.Claims;
+//using System.Security.Claims;
 using System.Security.Principal;
 using System.ServiceProcess;
 using System.Threading;
